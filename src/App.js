24054356bea3db0389f2ed5947c946aa4d3fdaef
Oluwajoba <oluwajobabello@gmail.com>
2019-09-24 02:50:59 +0100

    add 'DonorForm.jsx' to 'App.js' for rendering / App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import DonorForm from "./components/DonorForm";
+
 function App() {
   return (
     <div className="App">
       <p>render test</p>
+      <DonorForm />
     </div>
   );
 }
