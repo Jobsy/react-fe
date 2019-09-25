adabb88956e1859bfce72207265b4b5e43eaba16
Oluwajoba <oluwajobabello@gmail.com>
2019-09-25 03:16:12 +0100

    add campaign list and drop down input field / add spacing inbetween fields / DonorForm.jsx

--- src/components/DonorForm.jsx
+++ src/components/DonorForm.jsx
@@ -1,7 +1,48 @@
 
 import React, { useState, useEffect } from "react";
 import { Form as FormikForm, Field as FormikField } from "formik";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import { Button, Checkbox, Form, Dropdown } from "semantic-ui-react";
+
+
+
+const campaignOptions = [
+  {
+    key: 'Jenny Hess',
+    text: 'Jenny Hess',
+    value: 'Jenny Hess',
+    image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
+  },
+  {
+    key: 'Elliot Fu',
+    text: 'Elliot Fu',
+    value: 'Elliot Fu',
+    image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
+  },
+  {
+    key: 'Stevie Feliciano',
+    text: 'Stevie Feliciano',
+    value: 'Stevie Feliciano',
+    image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
+  },
+  {
+    key: 'Christian',
+    text: 'Christian',
+    value: 'Christian',
+    image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
+  },
+  {
+    key: 'Matt',
+    text: 'Matt',
+    value: 'Matt',
+    image: { avatar: true, src: '/images/avatar/small/matt.jpg' },
+  },
+  {
+    key: 'Justen Kitsune',
+    text: 'Justen Kitsune',
+    value: 'Justen Kitsune',
+    image: { avatar: true, src: '/images/avatar/small/justen.jpg' },
+  },
+]
 
 
 function DonorForm({ errors, touched, values, status }) {
@@ -36,6 +77,7 @@ function DonorForm({ errors, touched, values, status }) {
               {touched.fName && errors.fName && <p>{errors.fName}</p>}
             </label>
           </Form.Field>
+          <br />
 
           <Form.Field>
             <label>
@@ -48,6 +90,7 @@ function DonorForm({ errors, touched, values, status }) {
               {/* {touched.mName && errors.mName && <p>{errors.mName}</p>} */}
             </label>
           </Form.Field>
+          <br />
 
           <Form.Field>
             <label>
@@ -61,11 +104,13 @@ function DonorForm({ errors, touched, values, status }) {
             </label>
           </Form.Field>
         </Form.Group>
+        <br />
 
         <Form.Field label="Gender" control="select">
           <option value="female">Female</option>
           <option value="male">Male</option>
         </Form.Field>
+        <br />
 
         <Form.Field>
           <label>
@@ -78,27 +123,45 @@ function DonorForm({ errors, touched, values, status }) {
             {touched.email && errors.email && <p>{errors.email}</p>}
           </label>
         </Form.Field>
+        <br />
 
         <Form.Input label="Phone" type="phone" placeholder="+233 888 8888 888" />
+        <br />
+
         <Form.TextArea label="Home Address" placeholder="Your Billing/Contact Home Address" />
+        <br />
+
+        <Form.Field>
+          <label>
+            Campaign List:
+        <Dropdown
+              placeholder="Select Friend"
+              fluid
+              selection
+              options={campaignOptions}
+            />
+          </label>
+        </Form.Field>
+        <br />
 
         <Form.Group widths="equal">
           <Form.Input label="Donation" type="number" placeholder="Amount in Dollars" />
           <Form.Field>
-          <label>
-             <br/>
+            <label>
+              <br />
               <Button size="big" animated="fade">
-              <Button.Content visible onClick={toggle}>Donation Type</Button.Content>
-              <Button.Content hidden  onClick={toggle}>Check if Money</Button.Content>
-            </Button>
-            <Checkbox
-              label="Money or Others"
-              onChange={toggle}
-              checked={usersState.checked}
-            />
+                <Button.Content visible onClick={toggle}>Donation Type</Button.Content>
+                <Button.Content hidden onClick={toggle}>Check if Money</Button.Content>
+              </Button>
+              <Checkbox
+                label="Money or Others"
+                onChange={toggle}
+                checked={usersState.checked}
+              />
             </label>
           </Form.Field>
         </Form.Group>
+        <br />
 
         <Form.Field>
           <label>
@@ -108,13 +171,16 @@ function DonorForm({ errors, touched, values, status }) {
             {/* {touched.mList && errors.mList && <p>{errors.mList}</p>} */}
           </label>
         </Form.Field>
+        <br />
+        <br />
 
         <Form.Field>
-          <Button size="huge"animated="fade">
+          <Button size="huge" animated="fade">
             <Button.Content visible style={{ width: 100 }}>Submit</Button.Content>
             <Button.Content hidden>Click to Submit</Button.Content>
           </Button>
         </Form.Field>
+        <br />
 
         {/* <Display users={users} /> */}
       </Form>
